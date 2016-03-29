a51c58bea5df32f75e212e8ca2721815cb0ddc19
Megha Madan <mm6158@cs.nyu.edu>
2016-03-29 01:03:51 -0400

    assignment 7 added

--- drawlib5.js
+++ drawlib5.js
@@ -1,3 +1,18 @@
+   // DEFINE A CONVENIENT 3D VECTOR OBJECT.
+
+   function Vector3(x, y, z) {
+      this.x = 0;
+      this.y = 0;
+      this.z = 0;
+      this.set(x, y, z);
+   }
+   Vector3.prototype = {
+      set : function(x, y, z) {
+         if (x !== undefined) this.x = x;
+         if (y !== undefined) this.y = y;
+         if (z !== undefined) this.z = z;
+      },
+   }
 
    // KEEP TRACK OF TIME IN SECONDS SINCE THE PAGE WAS LOADED.
 
