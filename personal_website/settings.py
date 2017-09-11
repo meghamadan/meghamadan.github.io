f030127cf50fe33c5af78719773e36e279364cb8
Megha Madan <mm6158@cs.nyu.edu>
2017-09-10 20:25:47 -0400

    fix settings

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_URL = 'http://meghamadan.com'
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +26,7 @@ SECRET_KEY = 'a%p-0+sycbb9fo65_j#s5j6*rw9$^4@g$teha8m_m1vvmo6gwb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ 'www.meghamadan.com', 'www.megha.tech',  'meghamadan.github.io']
+ALLOWED_HOSTS = [ '*']
 
 
 # Application definition
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'personal_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- personal_website/urls.py
+++ personal_website/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('main_site.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('main_site.urls')),
     url(r'^graphics/', include('graphics_site.urls'))
 ]
